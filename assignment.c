008c9dbd3a65522f8474dd46d550823a28929f03
Jon <johntezjohn502@gmail.com>
2025-03-04 17:19:54 +0300

    fixed errors

--- assignment.c
+++ assignment.c
@@ -5,10 +5,12 @@ typedef struct{
 char name[10];
 int marks;
 char grade;
+
 }Student;//student structure for storing name and marks
 
+
 int main(){
-const int MAX_SIZE = sizeof(Student)*10; //declares constant size which we'll use for the array
+const int MAX_SIZE = 10; //declares constant size which we'll use for the array
 
 int i;
 
@@ -17,16 +19,17 @@ Student students[MAX_SIZE];//declares an array of structures
 printf("Enter names and marks of 10 students\n(SEPARATED BY A WHITESPACE eg: 'James 50',Marks between 1-100)\n");
 
 
-for(i=0;i<10;i++){
+for(i=0;i<3;i++){
 
 printf("Enter student's %d's NAME and MARKS; \n",i+1);
 scanf("%s %d",&students[i].name,&students[i].marks);
 
 }
 int grading(student_score){
-int grading_score = students[i].marks/10;
+int grading_score = student_score/10;
 
-switch(grading_score){
+switch(grading_score){ //switch case statement that grades students according to marks
+case 10:
 case 9:
 case 8:
   students[i].grade = 'A';
@@ -47,13 +50,13 @@ default:
 }
 }
 
-for(i=0;i<10;i++){
+for(i=0;i<3;i++){
   grading(students[i].marks);
 }
 
-for(i=0;i<10;i++){
+for(i=0;i<3;i++){ //Lists out the students names
   printf("%d student: name; %s marks; %d grade; %c\n",
-         i,students[i].name,students[i].marks,students[i].grade);
+         i+1,students[i].name,students[i].marks,students[i].grade);
 }
 
 return 0;
---
+++ booking/booking.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+
+typedef struct{
+ int room_number;
+ int is_occupied;
+
+}Room;
+
+
+typedef struct{
+char service_type[10];
+int pricing;
+
+}Service;
+
+int main(){
+  const Max_rooms = sizeof(Room) * 10;  const Max_services = sizeof(Service) * 10;
+  int i,x;
+  int prefered_room;
+
+  srand(time(NULL));//uses time as the seed for randomly generating values
+
+  Room rooms[Max_rooms];
+  Service services[10];
+
+  services_types[][10]
+
+  int booked_rooms(){ //random number genarator to randomly book rooms
+    for(i=0;i<10;i++){
+      rooms[i].is_occupied = rand() % 2;
+//      printf("%d\n",rooms[i].is_occupied);
+
+      if(rooms[i].is_occupied == 1){
+      printf("Room %d is booked\n",i+1);
+      }else{
+      printf("Room %d is vacant\n",i+1);
+      }
+    }
+  }
+
+  int booking(){
+    printf("Which room would you like to book: ");
+    scanf("%d",&prefered_room);
+    while(rooms[prefered_room-1].is_occupied != 0){
+      printf("Room occupied \nPlease select another one: ");
+      scanf("%d",&prefered_room);
+    }
+    printf("You have chosen room %d",prefered_room);
+
+    }
+
+
+booked_rooms();
+booking();
+return 0;
+}
