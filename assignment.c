5403a9ca1224953ec368d9063fe4102d479cbea8
Jon <johntezjohn502@gmail.com>
2025-03-06 21:24:53 +0300

    Readded switch 😅

--- assignment.c
+++ assignment.c
@@ -19,12 +19,12 @@ Student students[MAX_SIZE];//declares an array of structures
 printf("Enter names and marks of 10 students\n(SEPARATED BY A WHITESPACE eg: 'James 50',Marks between 1-100)\n");
 
 
-for(i=0;i<10;i++){
+for(i=0;i<3;i++){
 
 printf("Enter student's %d's NAME and MARKS; \n",i+1);
 scanf("%s %d",&students[i].name,&students[i].marks);
 
-while(students[i].marks <= 0 || students[i].marks >= 101){
+while(students[i].marks < 0 || students[i].marks >= 101){
   printf("Enter correct MARKS for student %d;\n",i+1);
   scanf("%d",&students[i].marks);
 
@@ -32,30 +32,39 @@ while(students[i].marks <= 0 || students[i].marks >= 101){
 
 
 }
-int grading(grading_score){
+int grading(grading_marks){
+ int grading_score = grading_marks/10;
+  // statements that grades students according to marks
+  switch(grading_score){
+    case 10:
+    case 9:
+    case 8:
+      students[i].grade = 'A';
+      break;
+    case 7:
+    case 6:
+      students[i].grade = 'B';
+      break;
+    case 5:
+      students[i].grade = 'C';
+      break;
+    case 4:
+      students[i].grade = 'D';
+      break;
+    default:
+      students[i].grade = 'E';
+      break;
 
-  //if statements that grades students according to marks
-if(grading_score >= 80 && grading_score <= 100){
-  students[i].grade = 'A';
+
+
+  }
+
 }
-else if(grading_score >= 60 && grading_score < 80){
-  students[i].grade = 'B';
-}
-else if(grading_score >= 40 && grading_score < 60){
-  students[i].grade = 'C';
-}
-else if(grading_score >= 30 && grading_score< 40){
-  students[i].grade = 'D';
-}
-else{
-  students[i].grade = 'E';
-}
-}
-for(i=0;i<10;i++){
+for(i=0;i<3;i++){
   grading(students[i].marks);
  }
 
-for(i=0;i<10;i++){ //Lists out the students names
+for(i=0;i<3;i++){ //Lists out the students names
   printf("%d student: name; %s marks; %d grade; %c\n",
          i+1,students[i].name,students[i].marks,students[i].grade);
 }
